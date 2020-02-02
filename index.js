8e17f06f696d9d01957d85903f1d040941327fbe
nihaprezz <serg13prez@outlook.com>
2020-02-01 23:13:47 -0500

    some ES6 random concepts

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 console.log('connected')
 
+//ES6 Spread operator
+const sum = (...args) => {
+    console.log(...args)
+    return args.reduce((a, b) => a + b, 0);
+  }
+
+console.log(sum(1, 2, 3)); // 6
+  
+
+//ES6 Class constructors
+class Car {
+    constructor(targetCar){
+        this.targetCar = targetCar
+    }
+}
+
+const toyota = new Car('toyota');
+console.log(toyota.targetCar)
